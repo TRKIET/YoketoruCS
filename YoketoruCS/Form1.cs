d40627d9a5311d223419d2ab7313118afddf0e20
trkiet <167387891+TRKIET@users.noreply.github.com>
2024-06-21 15:10:54 +0900

    状態定義

--- YoketoruCS/Form1.cs
+++ YoketoruCS/Form1.cs
@@ -2,9 +2,35 @@ namespace YoketoruCS
 {
     public partial class Form1 : Form
     {
+        enum State
+        {
+            None  = -1,
+            Title,
+            Game,
+            Gameover,
+            Clear,
+        }
+
+        //<summary>
+        //Ÿ‚ÉØ‚è‘Ö‚¦‚½‚¢ó‘Ô
+        //</summary>
+        State GetState = State.Title;
+
+        //<summry>
+        //Œ»İ‚Ìó‘Ô
+        //</summary>
+        State currentState = State.None;
+
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
